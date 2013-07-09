b7bc9ee2627b43f5f118d3b47bac1d4ac03c80fc
sksamuel <sam@sksamuel.com>
2013-07-09 15:16:42 +0100

    Added parameters to consumer

--- src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
+++ src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
@@ -1,22 +1,39 @@
 package org.apache.camel.component.rabbitmq;
 
-import org.apache.camel.Endpoint;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.apache.camel.Processor;
 import org.apache.camel.impl.DefaultConsumer;
+
+import java.io.IOException;
 
 /**
  * @author Stephen Samuel
  */
 public class RabbitMQConsumer extends DefaultConsumer {
 
-    public RabbitMQConsumer(Endpoint endpoint, Processor processor) {
+    private final RabbitMQEndpoint endpoint;
+
+    public RabbitMQConsumer(RabbitMQEndpoint endpoint, Processor processor) {
         super(endpoint, processor);
+        this.endpoint = endpoint;
     }
 
     @Override
     protected void doStart() throws Exception {
         super.doStart();
+        connect();
         log.info("Starting RabbitMQ consumer");
+    }
+
+    private void connect() throws IOException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(endpoint.getUsername());
+        factory.setPassword(endpoint.getPassword());
+        factory.setVirtualHost(endpoint.getVirtualHost());
+        factory.setHost(endpoint.getHostname());
+        factory.setPort(endpoint.getPortNumber());
+        Connection conn = factory.newConnection();
     }
 
     @Override
--- src/main/java/org/apache/camel/component/rabbitmq/RabbitMQEndpoint.java
+++ src/main/java/org/apache/camel/component/rabbitmq/RabbitMQEndpoint.java
@@ -5,19 +5,22 @@ import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
 
-import java.net.URI;
-
 /**
  * @author Stephen Samuel
  */
 public class RabbitMQEndpoint extends DefaultEndpoint {
+
+    private String username;
+    private String password;
+    private String virtualHost;
+    private String hostname;
+    private int portNumber;
 
     public RabbitMQEndpoint() {
     }
 
     public RabbitMQEndpoint(String endpointUri, String remaining, RabbitMQComponent component) throws Exception {
         super(endpointUri, component);
-        URI uri = new URI(remaining);
     }
 
     @Override
@@ -35,5 +38,45 @@ public class RabbitMQEndpoint extends DefaultEndpoint {
     @Override
     public boolean isSingleton() {
         return true;
+    }
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public String getVirtualHost() {
+        return virtualHost;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setVirtualHost(String virtualHost) {
+        this.virtualHost = virtualHost;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
     }
 }
