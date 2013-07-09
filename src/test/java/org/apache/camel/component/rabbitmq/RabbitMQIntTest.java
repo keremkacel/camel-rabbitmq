9f7c701b028493bc61cef54a083e001a8b6e8e68
sksamuel <sam@sksamuel.com>
2013-07-09 23:29:17 +0100

    Updated pom for int test profile

---
+++ src/test/java/org/apache/camel/component/rabbitmq/RabbitMQIntTest.java
@@ -0,0 +1,16 @@
+package org.apache.camel.component.rabbitmq;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @author Stephen Samuel
+ */
+public class RabbitMQIntTest {
+
+    @Test
+    public void fail() {
+        assertTrue(false);
+    }
+}
